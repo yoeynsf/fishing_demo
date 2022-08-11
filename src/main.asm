9748682ad21d553a6454431c230aa9bf465a393f
yoeynsf <yoey.nsf@gmail.com>
2022-08-10 21:35:51 -0600

    intro to fish

--- src/main.asm
+++ src/main.asm
@@ -9,6 +9,10 @@ mainprep:           ; any vars that need to be set up before can be done in here
     LDX #0			; NTSC
     JSR ft_music_init
 
+    LDX #35
+    LDY #1
+    JSR fadein_palette
+
 .proc main          ; "proc" is just a fancy way of saying scope (everything defined inside is not global)
 temp_x  = temp      ; defines for sprite loader. not interpreted as code or nuthin
 temp_y  = temp + 1
@@ -17,20 +21,6 @@ temp_y  = temp + 1
 	TAX
 	TAY
     JSR sprite_prep
-
-    LDA joy_status
-    AND #KEY_A      ; isolate the bit and see if it's pressed
-    BEQ :+
-    LDX #4
-    LDY #0
-    JSR fadein_palette
-:
-    LDA joy_status
-    AND #KEY_B 
-    BEQ :+
-    LDX #4
-    JSR fadeout_palette
-:
 
     JSR clear_sprites
 
--- src/rodata.asm
+++ src/rodata.asm
@@ -1,9 +1,20 @@
 palette_tablelow:               ; indexed into by the fade engine and whatever other palette thing. just add more entries
-    .byte <palette_data0
+    .byte <palette_data0, <palette_data1
 palette_tablehigh:
-    .byte >palette_data0
+    .byte >palette_data0, >palette_data1
     
 palette_data0:
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+
+palette_data1:
     .byte $0F, $15, $25, $35    ; BG
     .byte $0F, $16, $27, $37
     .byte $0F, $19, $29, $39
